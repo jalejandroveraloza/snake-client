b44c063880f711926b5c0fd5f676a321944b01c7
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-18 14:25:49 -0800

    ctrl c finish game

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 //const net = require("net");
 const connect = require('./client')
 
+const setUpInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+const handleUserInput = function () {
+  stdin.on('data', (key) => {
+    //process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
 
 console.log("Connecting ...");
 connect();
