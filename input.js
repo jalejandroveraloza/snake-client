a1ce5ab420e81aab21d43f02a8ee23e24a6bfc9f
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-20 20:44:59 -0800

    export functions to play.js

---
+++ input.js
@@ -0,0 +1,19 @@
+const setUpInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+const handleUserInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", (key) => {
+    //process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+
+module.exports = setUpInput;
