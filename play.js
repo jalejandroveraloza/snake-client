48f58b3ad32f0b991bb0b899397be4c12e010ec9
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2022-12-20 20:44:01 -0800

    calling setUpInput function

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 //const net = require("net");
-const connect = require('./client')
+const connect = require('./client');
+const setUpInput = require('./input');
+//const handleUserInput = require('./input')
 
-const setUpInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-}
-const handleUserInput = function () {
-  stdin.on('data', (key) => {
-    //process.stdout.write('.');
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-}
 
 console.log("Connecting ...");
 connect();
+
+setUpInput();
