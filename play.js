2396471222da97aa426725b30dccdbf2d214b0f7
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-01-02 13:50:53 -0800

    adding movement instructions from keyboard

--- play.js
+++ play.js
@@ -1,10 +1,10 @@
 //const net = require("net");
-const connect = require('./client');
-const setUpInput = require('./input');
+const {connect}  = require('./client');
+const {setUpInput}  = require('./input');
 //const handleUserInput = require('./input')
 
 
 console.log("Connecting ...");
-connect();
-
-setUpInput();
+//connect();
+//conn = connect()
+setUpInput(connect());
