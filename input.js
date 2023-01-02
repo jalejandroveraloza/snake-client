8e374b77d40cfb1154396d216f4bb9316bb10784
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-01-02 13:49:43 -0800

    adding movement instructions from keyboard

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setUpInput = function () {
+const {net, connect} = require('./client');
+let connection;//stores the active TCP connection object 
+
+const setUpInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,14 +10,32 @@ const setUpInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 }
-const handleUserInput = function () {
-  const stdin = process.stdin;
-  stdin.on("data", (key) => {
+const handleUserInput = function (key) {
+  //const stdin = process.stdin;
+ // stdin.on("data", (key) => {
     //process.stdout.write('.');
+    if(key === "w"){
+      connection.write('Move: up')
+      //console.log("Move: up")
+    }
+    if(key === "a"){
+      connection.write('Move: left')
+      //console.log("Move: left")
+    }
+    if(key === "s"){
+      connection.write('Move: down')
+      //console.log("Move: down")
+    }
+    if(key === "d"){
+      connection.write('Move: right')
+      //console.log("Move: rigth")
+    }
     if (key === '\u0003') {
       process.exit();
     }
-  });
-}
+  };
 
-module.exports = setUpInput;
+
+module.exports = {
+  setUpInput
+};
